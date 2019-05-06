ceb4bb6c392ab056354c5067a50683cdff6b0ab3
MarcusJones <jones.0bj3@gmail.com>
2019-05-06 15:26:29 +0200

    update verification script

--- verify.sh
+++ verify.sh
@@ -8,14 +8,58 @@ for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
 done 
 eval RESET='$reset_color' 
 
-echo "${BOLD_CYAN}***Python Version***${NOCOLOR}"
-python --version
+#############################
+PYTHON_CODE=$(cat <<END
+from tensorflow.python.client import device_lib
 
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+get_available_gpus()
+END
+)
+
+#############################
+
+pipenv shell
+
+#############################
+
+echo ""
+echo "${BOLD_CYAN}Linux version${NOCOLOR}"
 uname -m 
-cat /etc/*release
 uname -a
- 
- 
-### VI MODE #################################### 
-echo ${MAGENTA}vi mode enabled${RESET} 
+cat /etc/upstream-release/lsb-release
+echo ""
 
+echo "${BOLD_CYAN}GCC version${NOCOLOR}"
+gcc --version
+
+echo "${BOLD_CYAN}Video card version${NOCOLOR}"
+lspci | grep -i nvidia
+echo ""
+
+echo "${BOLD_CYAN}GPU nvcc version${NOCOLOR}"
+which nvcc
+nvcc --version
+echo ""
+
+echo "${BOLD_CYAN}Dynamic linker run-time bindings${NOCOLOR}"
+ldconfig -p | grep cuda
+echo ""
+
+echo "${BOLD_CYAN}Python Version${NOCOLOR}"
+python --version
+echo ""
+ 
+echo "${BOLD_CYAN}Library Path for CUDA${NOCOLOR}"
+echo $LD_LIBRARY_PATH  
+echo ""
+
+#############################
+echo "${BOLD_CYAN}Check python import and devices${NOCOLOR}"
+res="$(python3 -c "$PYTHON_CODE")"
+
+# continue with bash code
+echo "$res"

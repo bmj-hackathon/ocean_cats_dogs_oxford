f1541fcc72515aa0914de516e591d23b59a7e89d
MarcusJones <jones.0bj3@gmail.com>
2019-05-08 14:34:59 +0200

    add list image folder

--- src/1_data/data_00_load.py
+++ src/1_data/data_00_load.py
@@ -21,16 +21,33 @@ assert path_labels.exists()
 df = pd.read_csv(path_labels, header=6, delim_whitespace=True)
 df.columns = ['file name', 'class', 'species', 'breed']
 df.head()
-#%%
+# %%
 class AIDataSet():
-    def __init__(self, image_folder, df, target_col):
+    def __init__(self, image_folder, image_extension, df, col_file, col_target):
         """
 
         :param image_folder:
+        :param image_extension:
         :param df:
-        :param target_col:
+        :param col_target:
         """
         self.image_folder = image_folder
+        self.image_extension = image_extension
         self.df = df
-        self.target_col = target_col
-        
+        self.col_file = col_file
+        self.target_col = col_target
+
+        logging.debug("{} images found".format(len(self.list_images())))
+        logging.info("Dataframe with {} records, target column is {}".format(len(df), self.target_col))
+
+    def list_images(self):
+        image_list = self.image_folder.glob("*"+self.image_extension)
+        return list(image_list)
+
+# %%
+image_folder = path_data / "images"
+ds = AIDataSet(image_folder, ".jpg", df, 'class')
+res = ds.list_images()
+
+
+image_folder.glob("*."+self.image_extension)
